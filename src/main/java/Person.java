cbca69612858c20c4e8faa68d1d708140354c156
Maniaki68 <YUNAPOWAA59@gmail.com>
2022-01-14 19:34:01 +0100

    commit with dependencies

---
+++ src/main/java/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    String name;
+    String firstname;
+    String telephonenumber;
+    String adresse;
+
+    public Person(){
+        this.name = null;
+        this.firstname = null;
+        this.telephonenumber = null;
+        this.adresse = null;
+    }
+
+    public Person(String name,String firstname,String telephonenumber,String adresse){
+        this.name = name;
+        this.firstname = firstname;
+        this.telephonenumber = telephonenumber;
+        this.adresse = adresse;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setTelephonenumber(String telephonenumber){
+        this.telephonenumber = telephonenumber;
+    }
+    public void setAdresse(String adresse){
+        this.adresse = adresse;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getFirstname(){
+        return this.firstname;
+    }
+    public String getTelephonenumber(){
+        return this.telephonenumber;
+    }
+    public String getAdresse(){
+        return this.adresse;
+    }
+}
